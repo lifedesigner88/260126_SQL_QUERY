c9cfe2cab217458b41c41436dcbd191a0cf40048
Sejong Park <lifedesigner88@gmail.com>
2026-01-26 14:11:43 +0900

    기초

---
+++ sqlite/schema.sql
@@ -0,0 +1,22 @@
+-- Data Definition Language (DDL)
+DROP TABLE movies;
+
+CREATE TABLE movies (
+	movie_id	INTEGER PRIMARY KEY AUTOINCREMENT,
+	title 		TEXT 	NOT NULL UNIQUE ,
+	released	INTEGER NOT NULL CHECK (released > 0), -- 1, 2, 3 ....
+	overview 	TEXT 	NOT NULL CHECK (LENGTH(overview) <= 100), 
+	rating 		REAL 	NOT NULL CHECK (rating BETWEEN 0 AND 10),	-- 1.2 9.7
+	director 	TEXT,
+	for_kids 	INTEGER NOT NULL CHECK (for_kids BETWEEN 0 AND 1) DEFAULT 0 -- 0 or 1
+	-- poster BLOB
+) STRICT;
+
+
+INSERT INTO movies 
+	(title, rating, released, overview, for_kids)
+VALUES 			
+	('The Lord of Thed Rings', 4.22, 100, 'Rings and hobiitsRinddddddgiits', 0),
+	('Dune: Part One', 8, 100, 'Sand', 1);
+
+SELECT * FROM movies;
