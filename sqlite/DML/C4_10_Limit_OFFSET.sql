336ac21ab0c26fb54c81958f8761c4f4359a860a
SejongPark <lifedesigner88@gmail.com>
2026-01-28 16:33:30 +0900

    limit

--- sqlite/DML/C4_10_Limit_OFFSET.sql
+++ sqlite/DML/C4_10_Limit_OFFSET.sql
@@ -1,8 +1,11 @@
+
 SELECT -- 3
 	*
 FROM -- 1
 	movies
 WHERE -- 2
-	title LIKE 'The%'
-LIMIT 5 -- 5
-OFFSET 2 * 5 -- 4
+	title LIKE 'The%' AND release_date NOT NULL
+ORDER BY -- 4
+	release_date
+LIMIT 5 -- 6
+OFFSET 3 * 5 -- 5
