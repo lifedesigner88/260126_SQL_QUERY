d4514d664724cd2d67975841489e101e97ff0834
Sejong Park <lifedesigner88@gmail.com>
2026-02-01 18:00:04 +0900

    1

---
+++ sqlite/_5_SUBQUERY/C5_06_practice1.sql
@@ -0,0 +1,91 @@
+WITH director_stats AS (
+	SELECT 
+		director,
+		COUNT(*) AS total_movies,
+		AVG(rating) AS avg_rating,
+		MAX(rating) AS best_rating,
+		MIN(rating) AS worst_rating,
+		MAX(budget) AS highest_budget,
+		MIN(budget) AS lowest_budget
+	FROM
+		movies
+	WHERE
+		director IS NOT NULL
+		AND rating IS NOT NULL
+		AND budget IS NOT NULL
+		AND budget != 0
+	GROUP BY
+		director
+	-- HAVING total_movies > 2
+	-- LIMIT 20
+)
+
+SELECT
+	director 
+	total_movies,
+	avg_rating,
+	best_rating,
+	worst_rating,
+	highest_budget,
+	lowest_budget,
+	(
+		SELECT
+			title
+		FROM movies
+		WHERE
+			director IS NOT NULL
+			AND rating IS NOT NULL
+			AND budget IS NOT NULL
+			AND budget != 0
+			AND director == director_name.director
+		ORDER BY
+			rating DESC
+		LIMIT 1
+	) AS best_rated_movie,
+	(
+		SELECT
+			title
+		FROM movies
+		WHERE
+			director IS NOT NULL
+			AND rating IS NOT NULL
+			AND budget IS NOT NULL
+			AND budget != 0
+			AND director == director_name.director
+		ORDER BY
+			rating ASC
+		LIMIT 1
+	) AS worst_rated_movie,
+	(
+		SELECT
+			title
+		FROM movies
+		WHERE
+			director IS NOT NULL
+			AND rating IS NOT NULL
+			AND budget IS NOT NULL
+			AND budget != 0
+			AND director == director_name.director
+		ORDER BY
+			budget DESC
+		LIMIT 1
+	) AS most_expensive_movie,
+	(
+		SELECT
+			title
+		FROM movies
+		WHERE
+			director IS NOT NULL
+			AND rating IS NOT NULL
+			AND budget IS NOT NULL
+			AND budget != 0
+			AND director == director_name.director
+		ORDER BY
+			budget DESC
+		LIMIT 1
+	) AS least_expensive_movie
+	
+FROM 
+	director_stats as director_name;
+
+CREATE INDEX idx_director ON movies (director);
